a3fec67ddd194442a1944f42366885ba0a23a55d
boneill <brian.oneill@seedim.com.au>
2021-06-16 14:35:42 +1000

    Addd actions for view and to search

--- alfresco-content-app/projects/seedim-cart-ext/src/lib/containers/purchased-items.component/purchased-items.component.ts
+++ alfresco-content-app/projects/seedim-cart-ext/src/lib/containers/purchased-items.component/purchased-items.component.ts
@@ -1,9 +1,13 @@
 import { DocumentListComponent } from '@alfresco/adf-content-services';
 import { AlfrescoApiService, AuthenticationService } from '@alfresco/adf-core';
+import { MinimalNodeEntity } from '@alfresco/js-api';
 
 import { Component, OnInit } from '@angular/core';
+//import { Router } from '@angular/router';
 //import { ActivatedRoute } from '@angular/router';
 import { from, Observable} from 'rxjs';
+import { AppStore, ViewNodeAction,  ViewNodeExtras } from '@alfresco/aca-shared/store'; 
+import { Store } from '@ngrx/store';
 
 
 @Component({
@@ -23,8 +27,10 @@ export class PurchasedItemsComponent implements OnInit {
 
   constructor(
     private authService: AuthenticationService, 
-    // private route: ActivatedRoute,
-    private apiService: AlfrescoApiService ) { 
+    //private route: ActivatedRoute,
+    private apiService: AlfrescoApiService,
+    //private router: Router,
+    protected store: Store<AppStore> ) { 
 
       //this.userCartFolderNode$ = of<any>({id: null});
 
@@ -74,5 +80,31 @@ getUserCartFolder(userId: string): Observable <any>
     if($event)  
       this.isItemSelected = true;
 
+  }
+
+  navigateTo(node: MinimalNodeEntity) {
+   
+   console.log("Navigate to called for node", node);
+   
+    if (node && node.entry) {
+     
+      //this.showPreview(node, { location: '/cart/purchased-items' });
+      this.showPreview(node, {path: '/cart/purchased-items'});
+    }
+  }
+
+  showPreview(node: MinimalNodeEntity, extras?: ViewNodeExtras) {
+   
+   console.log("Show preview called for node ", node);
+    if (node && node.entry) {
+      let id: string;
+
+      
+        id = (node as any).entry.nodeId || (node as any).entry.guid || node.entry.id;
+      
+        console.log("Dispatching store action viewnodeaction ", id, extras)
+
+      this.store.dispatch(new ViewNodeAction(id, extras));
+    }
   }
 }
